1a364ec10404bd3a537eb60ea1f9b3de4136d824
Harvey <jjimenez@baunfire.com>
2026-04-10 07:54:28 +0800

    video embed component

---
+++ src/components/VideoEmbed.jsx
@@ -0,0 +1,33 @@
+export const VideoEmbed = ({ videoId, type = "youtube" }) => {
+  const videoSources = {
+    youtube: `https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&mute=1&playsinline=1`,
+    vimeo: `https://player.vimeo.com/video/${videoId}?autoplay=1&loop=1&muted=1&background=1`,
+  };
+
+  if (!videoId) {
+    return null;
+    }
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        position: 'relative'
+      }}
+    >
+      <iframe
+        src={videoSources[type]}
+        style={{
+            width: "100%",
+            height: "100%",
+            border: 0
+        }}
+        allow="autoplay; fullscreen"
+        allowFullScreen
+        title="Embedded Video"
+      />
+      <div style={{ position: 'absolute', zIndex: 2, width: '100%', height: '100%', inset: 0 }}></div>
+    </div>
+  );
+};
---
+++ src/components/VideoEmbed.webflow.jsx
@@ -0,0 +1,21 @@
+import { VideoEmbed } from './VideoEmbed';
+import { props } from '@webflow/data-types';
+import { declareComponent } from '@webflow/react';
+
+export default declareComponent(VideoEmbed, {
+  name: 'Video Embed',
+  description: 'Embed a YouTube or Vimeo video using a video ID',
+  group: 'Media',
+  props: {
+    type: props.Variant({
+      name: "Video Type",
+      options: ["youtube", "vimeo"],
+      defaultValue: "youtube",
+    }),
+    videoId: props.Text({
+      name: "Video ID",
+      defaultValue: "",
+      helpText: "Enter the YouTube or Vimeo video ID",
+    })
+  },
+});
