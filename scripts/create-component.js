57f83fb72bcf430d254549a09294f5961a46ca0a
Harvey <jjimenez@baunfire.com>
2026-04-10 21:29:50 +0800

    script to automatically create component

---
+++ scripts/create-component.js
@@ -0,0 +1,66 @@
+import inquirer from "inquirer";
+import fs from "fs-extra";
+import path from "path";
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function getComponentTemplate(name) {
+  return `export const ${name} = ({ text }) => {
+  return (
+    <div>
+    </div>
+  );
+};
+`;
+}
+
+function getWebflowTemplate(name) {
+  return `import { ${name} } from './${name}';
+import { props } from '@webflow/data-types';
+import { declareComponent } from '@webflow/react';
+
+export default declareComponent(${name}, {
+  name: '${name}',
+  description: '',
+  props: {
+    text: props.Text({
+      name: "Text",
+      defaultValue: "",
+      helpText: "",
+    })
+  },
+});
+`;
+}
+
+async function run() {
+  const { componentName } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "componentName",
+      message: "Enter component name (PascalCase):",
+    },
+  ]);
+
+  const name = capitalize(componentName.trim());
+  
+  const dir = path.join(process.cwd(), "src", "components");
+
+  await fs.ensureDir(dir);
+
+  await fs.writeFile(
+    path.join(dir, `${name}.jsx`),
+    getComponentTemplate(name)
+  );
+
+  await fs.writeFile(
+    path.join(dir, `${name}.webflow.jsx`),
+    getWebflowTemplate(name)
+  );
+
+  console.log(`Created ${name} in src/components`);
+}
+
+run();
