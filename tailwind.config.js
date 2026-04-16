7114daf4ff54a3787e3a7d143c1bb94eb43c6238
Samuel Philip Hayag <hhayag@baunfire.com>
2026-04-17 02:02:26 +0800

    text inherit for richtext

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,26 @@ export default {
   corePlugins: {
     preflight: false,
   },
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  safelist:[
-    "text-teal",
-    "text-white",
-    "text-blue",
-    "text-yellow"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: ["text-teal", "text-white", "text-blue", "text-yellow"],
   theme: {
     extend: {
-      colors:theme.colors
+      colors: theme.colors,
     },
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".text-inherit-all": {
+          "font-size": "inherit",
+          color: "inherit",
+          "font-weight": "inherit",
+          "line-height": "inherit",
+          "letter-spacing": "inherit",
+          "font-family": "inherit",
+          "text-align": "inherit",
+        },
+      });
+    },
+  ],
+};
