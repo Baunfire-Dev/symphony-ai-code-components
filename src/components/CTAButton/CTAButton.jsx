39ed94689719cb6db7c60f80605653e5b0fcc000
rcuer <rcuer@baunfire.com>
2026-04-19 01:56:50 +0800

    updates to build

--- src/components/CTAButton/CTAButton.jsx
+++ src/components/CTAButton/CTAButton.jsx
@@ -10,7 +10,8 @@ export const CTAButton = ({
     "Blue" : "blue",
     "Teal" : "teal",
     "Yellow" : "yellow",
-    "Light Blue" : "light-blue"
+    "Light Blue" : "light-blue",
+    "Light Purple" : "light-purple"
   }
   return (
     <a href={link?.href} target={link?.target ?? "_self"} className={`btn ${buttonVariant[variant]}`}>
