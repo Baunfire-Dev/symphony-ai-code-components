223d7be44e3f4784f64dd8c36b1fda76129612d3
Harvey <jjimenez@baunfire.com>
2026-05-04 21:19:23 +0800

    updates

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -25,5 +25,25 @@ export const Heading = ({ isVisible, textColor = "Default", tag: Tag = "h1", tex
   const headingAppearance = appearance && appearance !== "None" ? (appearanceMap[appearance] || appearance) : null;
   const alignment = alignmentMap[align];
 
-  return <Tag className={[headingAppearance, alignment, textColorMap[textColor], !isVisible ? "hidden" : ""].filter(Boolean).join(" ")}>{text}</Tag>;
+  const renderText = (text = "") => {
+    if (typeof text !== "string") return text;
+
+    if (!text.includes("[[")) return text;
+
+    return text.split(/(\[\[.*?\]\])/g).map((part, i) => {
+      if (part.startsWith("[[") && part.endsWith("]]")) {
+        const clean = part.replace(/\[\[|\]\]/g, "");
+
+        return (
+          <span key={i} className="gradient-text">
+            {clean}
+          </span>
+        );
+      }
+
+      return part;
+    });
+  };
+
+  return <Tag className={[headingAppearance, alignment, textColorMap[textColor], !isVisible ? "hidden" : ""].filter(Boolean).join(" ")}>{renderText(text)}</Tag>;
 };
