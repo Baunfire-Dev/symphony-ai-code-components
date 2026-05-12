34188c24db712be57f16f770fca7e81732283ffb
rcuer <rcuer@baunfire.com>
2026-05-13 05:53:49 +0800

    updates to build

--- src/components/ResourcesCards/ResourcesCards.mjs
+++ src/components/ResourcesCards/ResourcesCards.mjs
@@ -86,7 +86,7 @@ import { props } from '@webflow/data-types';
 import { declareComponent } from '@webflow/react';
 
 export default declareComponent(ResourcesCards, {
-    name: "Resource Block",
+    name: "ResourcesCards",
     group: "Resources",
     options: {
         ssr: false,
