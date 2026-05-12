c2ff1bde7b786ec3bc9da08ce212c6d7898225ab
rcuer <rcuer@baunfire.com>
2026-05-13 05:55:21 +0800

    updates to build

--- src/components/ResourcesCards/ResourcesCards.jsx
+++ src/components/ResourcesCards/ResourcesCards.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FILTER_MAP } from "./filterMap";
+import FILTER_MAP from "./filterMap";
 
 export default function ResourceBlock(props) {
     const { resourcesCollectionId = "", ...filterProps } = props;
