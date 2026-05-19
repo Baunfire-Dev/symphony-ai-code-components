c99deb5d1e2d7b8389c207e874540f58bb4229a3
rcuer <rcuer@baunfire.com>
2026-05-20 07:48:05 +0800

    updates to build

--- src/components/Heading/Heading.webflow.jsx
+++ src/components/Heading/Heading.webflow.jsx
@@ -3,43 +3,43 @@ import { props } from "@webflow/data-types";
 import { declareComponent } from "@webflow/react";
 
 export default declareComponent(Heading, {
-  name: "Heading",
-  description: "",
-  group: "Global",
-  props: {
-    isVisible: props.Visibility({
-      name: "Visible",
-      group: "Display",
-      defaultValue: true
-    }),
-    textColor: props.Variant({
-      name: "Text Color",
-      defaultValue: "Default",
-      options: ["Default", "White", "Teal", "Blue", "Yellow", "Light Purple", "Magenta", "Light Blue"],
-      tooltip: "Select text color",
-    }),
-    tag: props.Variant({
-      name: "Tag",
-      defaultValue: "h1",
-      options: ["h1", "h2", "h3", "h4", "h5", "h6"],
-      tooltip: "Select heading level",
-    }),
-    appearance: props.Variant({
-      name: "Appearance",
-      defaultValue: "",
-      options: ["None", "h1", "h2", "h3", "h4", "h5", "h6", "Callout Text"],
-      tooltip: "Select heading appearance",
-    }),
-    text: props.Text({
-      name: "Text",
-      defaultValue: "Heading",
-      tooltip: "Use [[text]] for gradient styling or {{text}} for highlight styling. Example: This is [[gradient text]] and this is {{highlighted text}}",
-    }),
-    align: props.Variant({
-      name: "Text Alignment",
-      defaultValue: "left",
-      options: ["left", "center", "right"],
-      tooltip: "Align heading text",
-    }),
-  },
+    name: "Heading",
+    description: "",
+    group: "Global",
+    props: {
+        isVisible: props.Visibility({
+            name: "Visible",
+            group: "Display",
+            defaultValue: true
+        }),
+        textColor: props.Variant({
+            name: "Text Color",
+            defaultValue: "Default",
+            options: ["Default", "White", "Teal", "Blue", "Yellow", "Light Purple", "Magenta", "Light Blue"],
+            tooltip: "Select text color",
+        }),
+        tag: props.Variant({
+            name: "Tag",
+            defaultValue: "h1",
+            options: ["h1", "h2", "h3", "h4", "h5", "h6"],
+            tooltip: "Select heading level",
+        }),
+        appearance: props.Variant({
+            name: "Appearance",
+            defaultValue: "",
+            options: ["None", "h1", "h2", "h3", "h4", "h5", "h6", "Callout Text"],
+            tooltip: "Select heading appearance",
+        }),
+        text: props.Text({
+            name: "Text",
+            defaultValue: "Heading",
+            tooltip: "Use [[text]] for gradient styling or {{text}} for highlight styling. Example: This is [[gradient text]] and this is {{highlighted text}}",
+        }),
+        align: props.Variant({
+            name: "Text Alignment",
+            defaultValue: "left",
+            options: ["left", "center", "right"],
+            tooltip: "Align heading text",
+        }),
+    },
 });
